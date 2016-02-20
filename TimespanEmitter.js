d9538a492a369b16e53f24f59fa686eedb534079
Christopher Cote <chris@entropealabs.com>
2016-02-19 21:11:25 -0600

    faster start, cleaned up test

--- TimespanEmitter.js
+++ TimespanEmitter.js
@@ -4,15 +4,14 @@
     function TimespanEmitter(start_time, end_time, interval, playback_rate, loop){
         this.start_time = typeof start_time !== 'number' ? start_time.getTime() : start_time;
         this.end_time = typeof end_time !== 'number' ? end_time.getTime() : end_time;
-        this.current = this.start_time;
         this.interval = interval;
         this.loop = loop || false;
         this.playback_rate = playback_rate;
-        this.last = performance.now();
         this.total_time = this.end_time-this.start_time;//18000000
         this.num_steps = Math.ceil(this.total_time/this.interval);//18000
         this.event_rate = (this.total_time/this.playback_rate)/this.num_steps;//1800000/18000
         this.step_increment = this.total_time/this.num_steps;
+        this.last = performance.now()-this.event_rate;
         this.counter = 0;
     };
 
